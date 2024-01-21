0c8647a5eb3fe57687dcfa85edbcfc238b98cdcb
burakgercekaslan <burakgercekaslan@gmail.com>
2024-01-21 21:07:05 +0300

    bird_added_rotation

--- Bülbül Uçuyor/Assets/Scripts/Bird_Jump.cs
+++ Bülbül Uçuyor/Assets/Scripts/Bird_Jump.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bird_Jump : MonoBehaviour
 {
     public float velocity = 1f;
+    public float _rotationspeed = 10f;
     public Rigidbody2D rb2D;
     void Update()
     {
@@ -12,5 +13,9 @@ public class Bird_Jump : MonoBehaviour
     {
         rb2D.velocity = Vector2.up * velocity;
     }
+    }
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0,0,_rotationspeed*rb2D.velocity.y);
     }
 }
